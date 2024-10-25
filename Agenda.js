b050d76c0a2b60dfa64f31fa4405ce375355cbde
Jonnthan3visan <joptrabajo@hotmail.com>
2024-10-24 22:34:46 -0300

    clase Agenda
    metodo recibeInfoDe funciona correctamente

--- Agenda.js
+++ Agenda.js
@@ -1,7 +1,18 @@
 class Agenda {
     #agendado = []
 
+    /**
+     * @returns agendados 
+     */
+    getAgendados() {
+        return this.#agendado
+    }
 
+
+    /**
+     * recibe la infomacion completa del cliente y lo agrega al array agendados
+     * @param {*} cliente 
+     */
     recibeInfoDe(cliente) {
         this.#agendado.push(cliente)
     }
--- app.js
+++ app.js
@@ -1,19 +1,25 @@
+const Agenda = require("./Agenda");
 const Cliente = require("./Cliente");
 const DBCliente = require("./DBCliente");
 const GeneradorID = require("./GeneradorID");
 const GestorArchivoTXT = require("./GestorArchivoTXT");
+const Reserva = require("./Reserva");
 //------------instancias necesarias-----------------
-const persona = new Cliente("Celete cid", "lewo@hot", "12456789")
+const persona = new Cliente("Brad Pit", "brat@hot", "14556789")
 const id_generador = new GeneradorID();
 const base_datos = new DBCliente();
 const gestor_txt = new GestorArchivoTXT();
-
+const reservas = new Reserva()
+const agendados = new Agenda();
 
 
 //--------------prueba de app-----------
 
 console.log(id_generador.getID());
 base_datos.guardarClienteDB(persona, gestor_txt, id_generador)
+reservas.solicitarInfoA(base_datos, agendados)
+console.log("agendado", agendados.getAgendados());
+
 
 
 
